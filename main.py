2c3acc189a9ca963a3feda8d107da7d5bad838b9
student <maksimkozlovskiy2011@gmail.com>
2023-04-25 19:56:42 +0300

    перший приклад від Дмитра

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#теорія
+
+#1
+
+a = 5
+b = 10
+
+def swap(a,b):
+    a,b = b,a
+    return a,b
+print(f"До обміну: a = {a} , b = {b}")
+a,b = swap(a,b)
+print(f"Після обміну: a = {a} , b = {b}")
+
+
+#2
+
+def count_vowels():
+    for i in s:
+        if s == "а" or "е" or ""
 
